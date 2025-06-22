b781a0cca87df7dd8837016576aed6c6786c0fcf
Federico Campero <89789363+frcampero@users.noreply.github.com>
2025-06-22 01:12:17 -0300

    Add new variable to OpenAI

--- ai.js
+++ ai.js
@@ -2,7 +2,7 @@ const { OpenAI } = require("openai");
 require("dotenv").config();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 async function interpretarGasto(text) {
@@ -20,19 +20,21 @@ Mensaje: "${text}"
       messages: [{ role: "user", content: prompt }],
       model: "gpt-3.5-turbo",
     });
-    
 
     let respuesta = completion.choices[0].message.content.trim();
 
-    if(respuesta.startsWith("```")){
-      respuesta = respuesta.replace(/```(?:json)?/g, "").replace(/```/g, "").trim()
+    if (respuesta.startsWith("```")) {
+      respuesta = respuesta
+        .replace(/```(?:json)?/g, "")
+        .replace(/```/g, "")
+        .trim();
     }
 
-    return JSON.parse(respuesta)
+    return JSON.parse(respuesta);
   } catch (err) {
     console.error("Error con la IA", err);
     return null;
   }
 }
 
-module.exports = { interpretarGasto }
+module.exports = { interpretarGasto };
